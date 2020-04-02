d1ac187bd4e6d49e1db7d49b72005959cc6263ea
Vladislav Hadzhiyski <vladislav.hadzhiyski@gmail.com>
2020-04-02 14:55:01 +0300

    Install ngrx packages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { AppMaterialModule } from './app-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +18,13 @@ import { AppComponent } from './app.component';
     FlexLayoutModule,
 
     AppMaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
