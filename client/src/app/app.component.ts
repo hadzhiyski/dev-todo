3f0a62e196e104d160ae46a77caedc3e58651a27
Vladislav Hadzhiyski <vladislav.hadzhiyski@gmail.com>
2020-04-03 15:08:35 +0300

    Layout centered. Make isHandset$ observable reusable in LayoutService

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,39 +1,17 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+import { Component } from '@angular/core';
 import { RxCleanupComponent } from '@devtodo/shared/components';
-import { fromEvent, Observable } from 'rxjs';
-import { map, shareReplay, takeUntil } from 'rxjs/operators';
+import { takeUntil } from 'rxjs/operators';
+import { LayoutService } from './shared/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent extends RxCleanupComponent implements AfterViewInit {
-  @ViewChild('mainContent')
-  mainContent: ElementRef;
-
-  @ViewChild('sidenav')
-  sidenav: MatSidenav;
-
-  constructor(private breakpointObserver: BreakpointObserver) {
+export class AppComponent extends RxCleanupComponent {
+  constructor(private layout: LayoutService) {
     super();
   }
 
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      takeUntil(this.componentDestroyed$),
-      map(result => result.matches),
-      shareReplay()
-    );
-
-  ngAfterViewInit(): void {
-    fromEvent(this.mainContent.nativeElement, 'click')
-      .pipe(takeUntil(this.componentDestroyed$))
-      .subscribe(() => {
-        this.sidenav.close();
-      });
-  }
+  isHandset$ = this.layout.isHandset$.pipe(takeUntil(this.componentDestroyed$));
 }
